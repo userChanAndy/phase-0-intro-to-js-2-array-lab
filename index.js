3849b6340ec9b4a8bc6e8da20b49b43847539713
Andy Chan <andy.chan9930@gmail.com>
2021-09-02 18:32:19 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(Broom){
+    var copyCats = cats.slice();
+    copyCats.push(Broom);
+    return copyCats
+}
+function prependCat(name){
+    var copyCats = cats.slice();
+    copyCats.unshift(name);
+    return copyCats
+}
+function removeLastCat(){
+    var copyCats = cats.slice();
+    copyCats.pop();
+    return copyCats
+}
+function removeFirstCat(){
+    var copyCats = cats.slice();
+    copyCats.shift();
+    return copyCats
+}
